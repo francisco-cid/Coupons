36bf0770c76c7f69f1086659c726c8136b776760
Francisco Cid <francisco.3400@yahoo.com>
2021-11-08 16:27:24 -0600

    changed flask port to 8080

--- main.py
+++ main.py
@@ -24,4 +24,4 @@ def redeem():
     return make_response({'coupons':coupons})
 
 if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=8000, threaded=True, debug=True)
+    app.run(host='0.0.0.0', port=8080, threaded=True, debug=True)
