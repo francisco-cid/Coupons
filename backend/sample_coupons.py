3bba8cfe928e9179440520ee4c8c0c05072e66b2
Francisco Cid <francisco.3400@yahoo.com>
2021-11-09 13:26:35 -0600

    added extra coupon cause spoilers

--- backend/sample_coupons.py
+++ backend/sample_coupons.py
@@ -14,54 +14,61 @@ coupon2 = {"id":2, "title":"Esther's Lasagna", "desc":"Esther will make you a la
     "ptime":"10 Nov 2021 12:00:00 GMT-0600"
     }
 
-coupon3 = {"id":3, "title":"Esther's Nopales", "desc":"Esther will make you a nopales salad", "request_by":"7 days",
+coupon3 = {"id":3, "title":"Dinner on me!", "desc":"Let's grab some grub on my dime", "request_by":"1 hour",
+    "used":False, "color":"danger",
+    "image":"https://www.clipartmax.com/png/full/12-126689_featured-comedy-sticky-short-farcical-comedy-about-fancy-dinner-table-clipart.png",
+    "ptime":"10 Nov 2021 12:00:00 GMT-0600"
+    }
+
+coupon4 = {"id":4, "title":"Esther's Nopales", "desc":"Esther will make you a nopales salad", "request_by":"7 days",
     "used":False, "color":"success",
     "image":"https://www.clipartmax.com/png/full/0-3121_black-clipart-cactus-cactus-black-and-white.png",
     "ptime":"12 Nov 2021 12:00:00 GMT-0600"
     }
 
-coupon4 = {"id":4, "title":"Morning Butler",
+coupon5 = {"id":5, "title":"Morning Butler",
     "desc":"I will wake up before you and make you breakfast, serve you coffee, and do anything else you'd want me to do",
     "request_by":"12 hours", "used":False, "color":"danger",
     "image":"https://www.clipartmax.com/png/full/1-13065_coffee-cup-clip-art-coffee-cup-clip-art.png",
     "ptime":"14 Nov 2021 12:00:00 GMT-0600"
     }
 
-coupon5 = {"id":5, "title":"Homework Help", "desc":"I will dedicate 4 hours to help you on a school assignment of your choice",
+coupon6 = {"id":6, "title":"Homework Help", "desc":"I will dedicate 4 hours to help you on a school assignment of your choice",
     "request_by":"3 days", "used":False, "color":"warning",
     "image":"https://www.clipartmax.com/png/full/14-145197_homework-homework-icon-black-and-white.png",
     "ptime":"16 Nov 2021 12:00:00 GMT-0600"
     }
 
-coupon6 = {"id":6, "title":"Clean Kitchen", "desc":"I will wash dishes, wipe surfaces, and sweep your kitchen",
+coupon7 = {"id":7, "title":"Clean Kitchen", "desc":"I will wash dishes, wipe surfaces, and sweep your kitchen",
     "request_by":"24 hours", "used":False, "color":"info",
     "image":"https://www.clipartmax.com/png/full/89-899344_cleaning-spray-gun-comments-clip-art-clean-spray.png",
     "ptime":"19 Nov 2021 12:00:00 GMT-0600"
     }
 
-coupon7 = {"id":7, "title":"Clean Bathroom", "desc":"I will scrub your shower, clean your toilet and sinks, and vacuum your bathroom",
+coupon8 = {"id":8, "title":"Clean Bathroom", "desc":"I will scrub your shower, clean your toilet and sinks, and vacuum your bathroom",
     "request_by":"24 hours", "used":False, "color":"light",
     "image":"https://www.clipartmax.com/png/full/37-378368_how-to-start-potty-training-a-resource-guide-my-bored-clip-art.png",
     "ptime":"20 Nov 2021 12:00:00 GMT-0600"
     }
 
-coupon8 = {"id":8, "title":"Starbucks Run", "desc":"I will wake up early and get you a starbucks drink for when you wake up",
+coupon9 = {"id":9, "title":"Starbucks Run", "desc":"I will wake up early and get you a starbucks drink for when you wake up",
     "request_by":"12 hours", "used":False, "color":"dark",
     "image":"https://www.clipartmax.com/png/full/102-1021317_additionally-starbucks-constant-engagement-with-fans-starbucks-gift-card-value.png",
     "ptime":"22 Nov 2021 12:00:00 GMT-0600"
     }
 
-coupon9 = {"id":9, "title":"Clean your fridge", "desc":"I will throw out anything that's gone bad, and wipe down surfaces in your fridge",
+coupon10 = {"id":10, "title":"Clean your fridge", "desc":"I will throw out anything that's gone bad, and wipe down surfaces in your fridge",
     "request_by":"24 hours", "used":False, "color":"primary",
     "image":"https://www.clipartmax.com/png/full/117-1176506_stainless-steel-fridge-sprite-015-club-penguin-fridge.png",
     "ptime":"24 Nov 2021 12:00:00 GMT-0600"
     }
 
-coupon10 = {"id":10, "title":"Inwood Sleepover", "desc":"I will spend the night at inwood with you", "request_by":"8 hours",
+coupon11 = {"id":11, "title":"Inwood Sleepover", "desc":"I will spend the night at inwood with you", "request_by":"8 hours",
     "used":False, "color":"success",
     "image":"https://www.clipartmax.com/png/full/2-21103_home-house-silhouette-icon-building-transparent-background-home-icon.png",
     "ptime":"26 Nov 2021 12:00:00 GMT-0600"
     }
+
 
 sample_coupons = []
 def main():
@@ -75,6 +82,7 @@ def main():
     sample_coupons.append(coupon8)
     sample_coupons.append(coupon9)
     sample_coupons.append(coupon10)
+    sample_coupons.append(coupon11)
 main()
 
 
